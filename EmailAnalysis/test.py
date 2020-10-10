ea48f3529da3d3b5f940f0eb24eac17509443aa4
Kamin Bouguyon <kamin.bouguyon@utdallas.edu>
2020-10-10 12:24:03 -0500

    added flask server to analysis service

--- EmailAnalysis/test.py
+++ EmailAnalysis/test.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from flask import Flask
 import requests
 import json
 import time
 from collections import Counter
 
-while(True):
+app = Flask(__name__)
 
+def process_emails():
   response = requests.get('http://cluster5-2.utdallas.edu:8080/api/queryEmails')
-  
+
   json_array = json.loads(response.text)
   email_list = []
-  
+
+  return_string = ""
+
   for item in json_array:
     email_details = {"id": None, "sender": None, "subject": None, "content": None, "is_spam": None}
     email_details['id'] = item['id']
@@ -21,16 +25,13 @@ while(True):
     email_details['content'] = item['content']
     email_details['is_spam'] = item['is_spam']
     email_list.append(email_details)
-    
-  #print(email_list[1]['content'])
-  
-  
+
   spamCounter = 0
   hamCounter = 0
   spamWordCounter = 0
   hamWordCounter = 0
-  
-  
+
+
   for item in email_list:
     if item['is_spam'] == False:
       hamCounter = hamCounter +1;
@@ -38,26 +39,26 @@ while(True):
     else:
       spamCounter = spamCounter +1;
       spamWordCounter += len(item['content'].strip().split(" "))
-    
+
   totalEmails = spamCounter + hamCounter
-  print("Total of " + str(totalEmails) + " emails, " + str(spamCounter) + " are spams")
-  print("Average word count of normal emails is: " + str(hamWordCounter/hamCounter) + ", average word count of spam emails is: " + str(spamWordCounter/spamCounter) + "\n")
-  
+  return_string += f"Total of {str(totalEmails)} emails, {str(spamCounter)} are spams\n"
+  return_string += f"Average word count of normal emails is: {str(hamWordCounter/hamCounter)}, average word count of spam emails is: {str(spamWordCounter/spamCounter)}\n\n"
+
   #######################################################################################
   stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the", "Subject:", "subject"]
-  
+
   spam_list = []
   ham_list = []
-  
+
   spamWords = {}
   hamWords = {}
-  
+
   for item in email_list:
     if item['is_spam'] == False:
       ham_list.append(item)
     else:
       spam_list.append(item)
-      
+
   for item in spam_list:
     wordcount = Counter(item['content'].split())
     for word in wordcount:
@@ -65,15 +66,14 @@ while(True):
         spamWords[word] = spamWords[word] + wordcount[word]
       else:
         spamWords[word] = wordcount[word]
-  
+
   sort_orders = sorted(spamWords.items(), key=lambda x: x[1], reverse=True)
-  print("the top 10 most common words in spam emails after removing stopwords, and numbers")
+  return_string += "the top 10 most common words in spam emails after removing stopwords, and numbers\n"
   for item in sort_orders[:10]:
-    print(item)
-  
-  
-  print(" " + "\n")
-  
+    return_string += f"{item}\n"
+
+  return_string += "\n"
+
   #####
   for item in ham_list:
     wordcount = Counter(item['content'].split())
@@ -82,16 +82,18 @@ while(True):
         hamWords[word] = hamWords[word] + wordcount[word]
       else:
         hamWords[word] = wordcount[word]
-  
+
   sort_orders = sorted(hamWords.items(), key=lambda x: x[1], reverse=True)
-  print("the top 10 most common words in normal emails after removing stopwords, and numbers")
+  return_string += "the top 10 most common words in normal emails after removing stopwords, and numbers\n"
   for item in sort_orders[:10]:
-    print(item)
-    
-  time.sleep(30)
+    return_string += f"{item}\n"
+
+  return return_string
 
 
+@app.route('/')
+def get_stats():
+    return process_emails()
 
-    
-
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6001)
