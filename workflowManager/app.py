54c9f447bf08d30594b4eea3b6d6023372c7c039
Kamin Bouguyon <kamin.bouguyon@utdallas.edu>
2020-11-06 23:12:52 -0700

    add workflow ids

--- workflowManager/app.py
+++ workflowManager/app.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
-from flask import Flask, request
 import docker
 from docker.types import EndpointSpec
+from flask import Flask, request
+import uuid
 
 app = Flask(__name__)
 
 client = docker.from_env()
 replicas = 3
 
-deployed_services = []
+deployed_workflows = []
+
+def deploy_component(component):
+    endpoint_spec = None
+    if "port" in component:
+        port = component.pop("port")
+        endpoint_spec = EndpointSpec(ports={port:port})
+    client.services.create(**component, endpoint_spec=endpoint_spec).scale(replicas=replicas)
+    print(f'Started service {component["image"]} with {replicas} replicas')
 
 def deployed(object):
+    deployed_services = client.services.list()
     for service in deployed_services:
         if service["image"] == object["image"]:
             return True
     return False
+
 
 @app.route('/start', methods=['POST'])
 def start_workflow():
@@ -23,29 +34,18 @@ def start_workflow():
 
         data = request.get_json(force=True)  # always try to parse data as JSON
 
+        workflow_id = uuid.uuid4()
         workflow_list = data['workflow']
         print(f'workflow list: {workflow_list}')
 
-        already_deployed = []
-
         for component in workflow_list:
             if not deployed(component):
-                endpoint_spec = None
-                if "port" in component:
-                    port = component.pop("port")
-                    endpoint_spec = EndpointSpec(ports={port:port})
-                client.services.create(**component, endpoint_spec=endpoint_spec).scale(replicas=replicas)
-                deployed_services.append(component)
-                print(f'Started service {component["image"]} with {replicas} replicas')
-            else:
-                already_deployed.append(component["image"])
-                print(f'Service {component["image"]} has already been started')
+                deploy_component(compnent)
+
+        deployed_workflows.append({ "id": workflow_id, "workflow": workflow_list })
 
         print('Data Received: "{data}"'.format(data=data))
-        if already_deployed:
-            return f"Services were already deployed: {already_deployed}\n"
-        return "Request Processed.\n"
-
+        return workflow_id
 
 
 if __name__ == '__main__':
