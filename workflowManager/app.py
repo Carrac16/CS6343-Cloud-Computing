a98f3ef8c6d2d7e74bc0dfc0fff2e21fd0907285
Kamin Bouguyon <kaminbouguyon@utdallas.edu>
2020-11-24 21:21:35 -0600

    update workflow manager to accept id's of previously started workflows

--- workflowManager/app.py
+++ workflowManager/app.py
@@ -12,12 +12,6 @@ replicas = 3
 
 deployed_workflows = {}
 
-def get_workflow(wfid):
-    for workflow in deployed_workflows:
-        if workflow['id'] == wfid:
-            return workflow
-    return False
-
 def deploy_component(component):
     endpoint_spec = None
     if "port" in component:
@@ -30,11 +24,11 @@ def deployed(object):
     return bool(client.services.list(filters={ "name": object["name"] }))
 
 def get_next_services(wfid, current):
-    workflow = deployed_workflows[wfid] # get_workflow(wfid)
+    workflow = deployed_workflows[wfid]
     if not workflow:
         return []
 
-    for component in workflow:
+    for component in workflow['components']:
         if component['service']['name'] == current:
             return component['next']
 
@@ -46,20 +40,30 @@ def start_workflow():
     if request.method == 'POST':
 
         data = request.get_json(force=True)  # always try to parse data as JSON
-        workflow = data['workflow']
-        workflow_id = str(uuid.uuid4())
-        entrypoint = workflow['entrypoint']
-        workflow_list = workflow['components']
-        print(f'workflow list: {workflow_list}')
+        if 'workflow_id' in data:
+            wfid = data['workflow_id']
+            if wfid in deployed_workflows:
+                return deployed_workflows[wfid]['entrypoint']
+            else:
+                return { "error": f"Workflow with ID ({wfid}) does not exist" }
 
-        for component in workflow_list:
-            service = component['service']
-            if not deployed(service):
-                deploy_component(service)
+        if 'workflow' in data:
+            workflow = data['workflow']
+            workflow_id = str(uuid.uuid4())
+            entrypoint = workflow['entrypoint']
+            workflow_list = workflow['components']
+            print(f'workflow list: {workflow_list}')
 
-        deployed_workflows[workflow_id] = workflow_list
+            for component in workflow_list:
+                service = component['service']
+                if not deployed(service):
+                    deploy_component(service)
 
-        return { "workflow_id": workflow_id, "entrypoint": entrypoint }
+            deployed_workflows[workflow_id] = { "components": workflow_list, "entrypoint": entrypoint }
+
+            return { "workflow_id": workflow_id, "entrypoint": entrypoint }
+        
+        return { "error": "User must specify a workflow or a workflow ID" }
 
 
 @app.route('/next', methods=['POST'])
